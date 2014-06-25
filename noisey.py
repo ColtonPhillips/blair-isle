77249b180851eb80961f36d5c028adad62de8e0d
Colton Phillips <coltonjphillips@gmail.com>
2014-06-24 19:03:17 -0700

    default message: fast commit

---
+++ noisey.py
@@ -0,0 +1,13 @@
+import random
+import math
+
+random.seed(0)
+
+def generateWhiteNoise(width,height):
+    noise = [[r for r in range(width)] for i in range(height)]
+
+    for i in range(0,height):
+        for j in range(0,width):
+            noise[i][j] = random.randint(0,1)
+
+    return noise
---
+++ test.py
@@ -0,0 +1,28 @@
+import sys, pygame, noisey, random
+pygame.init()
+surface_size= surface_width, surface_height = int(sys.argv[1]),int(sys.argv[2])
+size = width, height = 320,320 
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+my_surface = pygame.Surface(surface_size)
+
+def rando():
+	return int(random.uniform(1,255))
+
+def noisyColor(n):
+	return pygame.Color(rando()*n,rando()*n,rando()*n,1)
+
+def pixel(surface, color, pos):
+	surface.fill(color, (pos, (1, 1)))
+
+
+screen.fill(black)
+pygame.display.flip()
+my_surface.blit(screen, (0,0))
+
+my_noise = noisey.generateWhiteNoise(surface_width, surface_height)
+for i in range(0,surface_height):
+	for j in range(0,surface_width):
+		pixel(my_surface, noisyColor(my_noise[i][j]), (i,j))
+pygame.image.save(my_surface,"test.png")
