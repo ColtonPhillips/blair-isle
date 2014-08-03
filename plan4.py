2de47f1a2aa7bc1ad4cdcba0e7b6a483862d9a1e
Colton Phillips <coltonjphillips@gmail.com>
2014-08-02 19:10:21 -0600

    default message: fast commit

--- plan4.py
+++ plan4.py
@@ -14,7 +14,7 @@ def get_cool_surface(noise):
 	surf = pygame.Surface((len(noise),len(noise[0])))
 	surf.convert()
 	surf.convert_alpha()
-	surf.set_alpha(10)
+	surf.set_alpha(40)
 	for i in range(0,len(noise[0])):
 		for j in range(0,len(noise)):
 			pixel(surf, hotness(noise[i][j]), (j,i))
@@ -23,10 +23,20 @@ def get_cool_surface(noise):
 
 my_noise = noisey.generateWhiteNoise(surface_width, surface_height)
 my_surface = get_cool_surface(my_noise)
-for i in range(1,100):
+#for i in range(1,100):
+#	my_small_noise = noisey.generateWhiteNoise(i,i)
+#	surf = get_cool_surface(my_small_noise)
+#	surf = pygame.transform.scale(surf,(surface_width,surface_height))
+#	my_surface.blit(surf,(0,0))
+#
+i = 1
+while (i < surface_width/2):
 	my_small_noise = noisey.generateWhiteNoise(i,i)
 	surf = get_cool_surface(my_small_noise)
 	surf = pygame.transform.scale(surf,(surface_width,surface_height))
 	my_surface.blit(surf,(0,0))
+	i = i * 2
+
+
 
 pygame.image.save(my_surface,blairtool.out_file_name())
