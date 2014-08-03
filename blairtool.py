5a8fdb524af9ddd86450b055db407a7010967e63
Colton Phillips <coltonjphillips@gmail.com>
2014-08-02 22:15:16 -0600

    default message: fast commit

--- blairtool.py
+++ blairtool.py
@@ -1,21 +1,32 @@
+import pygame, random
+
+def off_or_on_color(val):
+	if val == 0: return pygame.Color(255,255,255,255)
+	else: return pygame.Color(0,0,0,255)
+
+def random_rgba_val():
+	return int(random.uniform(0,255))
+
+def random_color():
+	return pygame.Color(random_rgba_val(),random_rgba_val(),random_rgba_val(),255)
+
 def out_file_name():
 	import inspect
 	# this is gonna work on my machine only
 	return "out/" + inspect.stack()[-1][1].replace('./', "").replace(".\\", "").replace(".py",".png")
 
 def init_pygame():
-	import pygame, sys
+	import sys
+	random.seed()
 	pygame.init()
 	surface_size= surface_width, surface_height = int(sys.argv[1]),int(sys.argv[2])
 	size = width, height = 320,320 
 	black = 0, 0, 0
 	screen = pygame.display.set_mode(size)
-	screen.convert()
-	screen.convert_alpha()
 	my_surface = pygame.Surface(surface_size)
-	my_surface.convert_alpha()
 	screen.fill(black)
 	pygame.display.flip()
+	
 	return my_surface, surface_width, surface_height
 
 
--- plan4.py
+++ plan4.py
@@ -3,9 +3,6 @@ import sys, pygame, noisey, random, blairtool
 random.seed()
 
 my_surface, surface_width, surface_height = blairtool.init_pygame()
-def hotness(val):
-	if val == 0: return pygame.Color(255,255,255,100)
-	else: return pygame.Color(0,0,0,100)
 
 def pixel(surface, color, pos):
 	surface.fill(color, (pos, (1, 1)))
@@ -17,18 +14,22 @@ def get_cool_surface(noise):
 	surf.set_alpha(40)
 	for i in range(0,len(noise[0])):
 		for j in range(0,len(noise)):
-			pixel(surf, hotness(noise[i][j]), (j,i))
+			if (noise[i][j] != 0):
+				pixel(surf, blairtool.random_color(), (j,i))
 	return surf
 
 
-my_noise = noisey.generateWhiteNoise(surface_width, surface_height)
+my_noise = noisey.generateWhiteNoise(2, 2)
 my_surface = get_cool_surface(my_noise)
+my_surface = pygame.transform.scale(my_surface, (surface_width, surface_height))
+#MORE BORING 
 #for i in range(1,100):
 #	my_small_noise = noisey.generateWhiteNoise(i,i)
 #	surf = get_cool_surface(my_small_noise)
 #	surf = pygame.transform.scale(surf,(surface_width,surface_height))
 #	my_surface.blit(surf,(0,0))
 #
+
 i = 1
 while (i < surface_width/2):
 	my_small_noise = noisey.generateWhiteNoise(i,i)
@@ -36,7 +37,5 @@ while (i < surface_width/2):
 	surf = pygame.transform.scale(surf,(surface_width,surface_height))
 	my_surface.blit(surf,(0,0))
 	i = i * 2
-
-
 
 pygame.image.save(my_surface,blairtool.out_file_name())
